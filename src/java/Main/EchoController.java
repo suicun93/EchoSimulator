08a565c36a3f2a624d4c0d0b537789f532ebd301
Hoang Trung Duc <duchoang191@gmail.com>
2019-12-05 11:31:43 +0900

    Remove Thread and refactor code

--- src/java/Main/EchoController.java
+++ src/java/Main/EchoController.java
@@ -6,7 +6,6 @@
 package Main;
 
 import Common.Config;
-import Common.Constants;
 import Common.Convert;
 import Model.MyBattery;
 import Model.MyElectricVehicle;
@@ -16,11 +15,7 @@ import com.sonycsl.echo.Echo;
 import com.sonycsl.echo.EchoProperty;
 import com.sonycsl.echo.eoj.EchoObject;
 import com.sonycsl.echo.eoj.device.DeviceObject;
-import com.sonycsl.echo.eoj.device.housingfacilities.Battery;
 import com.sonycsl.echo.eoj.device.housingfacilities.ElectricVehicle;
-import com.sonycsl.echo.eoj.device.housingfacilities.GeneralLighting;
-import com.sonycsl.echo.eoj.device.housingfacilities.HouseholdSolarPowerGeneration;
-import com.sonycsl.echo.eoj.device.managementoperation.Controller;
 import com.sonycsl.echo.eoj.profile.NodeProfile;
 import com.sonycsl.echo.node.EchoNode;
 import com.sonycsl.echo.processing.defaults.DefaultNodeProfile;
@@ -28,7 +23,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Timer;
-import java.util.TimerTask;
 
 /**
  *
@@ -103,25 +97,9 @@ public class EchoController {
 //            System.out.println("Load config failed: " + e.getMessage());
 //        }
 //    }
-    static TimerTask notifyTask = null;
 
     public static void start(DeviceObject device) throws IOException {
         // notify to others
-        if (notifyTask == null) {
-            notifyTask = new TimerTask() {
-                @Override
-                public void run() {
-                    try {
-                        if (Echo.isStarted()) {
-                            NodeProfile.informG().reqInformInstanceListNotification().send();
-                        }
-                    } catch (IOException ex) {
-                        System.out.println(ex.getMessage());
-                    }
-                }
-            };
-            new Timer().scheduleAtFixedRate(notifyTask, 0, Constants.PERIOD);
-        }
         if (listDevice().contains(device)) {
             return;
         }
@@ -155,15 +133,11 @@ public class EchoController {
                 Echo.getSelfNode().removeDevice(device);
                 device.removeNode();
                 NodeProfile.informG().reqInformInstanceListNotification().send();
-                if (Echo.getSelfNode().getDevices().length == 0) {
-                    Echo.clear();
-                }
                 saveConfig();
             }
         } catch (IOException e) {
             throw new IOException("Stop: " + e.getMessage());
         }
-
     }
 
     public static boolean contains(String device) {
@@ -180,39 +154,6 @@ public class EchoController {
             return listDevice().contains(LIGHT);
         }
         return false;
-    }
-
-    // Device Detection
-    private static String deviceDetect(DeviceObject device) {
-        // Device detection
-        if (device instanceof Controller) {
-            return "Controller";
-        }
-        if (device instanceof MyBattery) {
-            return "My Battery";
-        }
-        if (device instanceof MyElectricVehicle) {
-            return "My Electric Vehicle";
-        }
-        if (device instanceof MySolar) {
-            return "My Solar";
-        }
-        if (device instanceof MyLight) {
-            return "My Light";
-        }
-        if (device instanceof ElectricVehicle) {
-            return "Electric Vehicle";
-        }
-        if (device instanceof Battery) {
-            return "Battery";
-        }
-        if (device instanceof HouseholdSolarPowerGeneration) {
-            return "Solar";
-        }
-        if (device instanceof GeneralLighting) {
-            return "Light";
-        }
-        return "Unknown";
     }
 
     // Device Object List to array
@@ -271,5 +212,4 @@ public class EchoController {
             }
         }); // No more events.
     }
-
 }
