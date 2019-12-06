b8d004bb77d9ed2b8b3e201d9d3c9165b19f81af
Hoang Trung Duc <duchoang191@gmail.com>
2019-12-06 11:06:42 +0900

    Add Inform

--- src/java/Model/MyDeviceObject.java
+++ src/java/Model/MyDeviceObject.java
@@ -33,6 +33,36 @@ public abstract class MyDeviceObject extends DeviceObject {
         // </editor-fold>
     }
 
+    public static enum OperationMode {
+        Other((byte) 0x40),
+        RapidCharging((byte) 0x41),
+        Charging((byte) 0x42),
+        Discharging((byte) 0x43),
+        Standby((byte) 0x44),
+        Test((byte) 0x45),
+        Automatic((byte) 0x46),
+        Idle((byte) 0x47),
+        Restart((byte) 0x48),
+        EffectiveCapacityRecalculationProcessing((byte) 0x49);
+
+        // <editor-fold defaultstate="collapsed" desc="//Skip this">
+        public final byte value;
+
+        public static OperationMode from(byte value) {
+            for (OperationMode mode : values()) {
+                if (mode.value == value) {
+                    return mode;
+                }
+            }
+            return Other;
+        }
+
+        OperationMode(byte operation) {
+            this.value = operation;
+        }
+        // </editor-fold>
+    }
+
     public String getAddressStr() {
         return getNode().getAddressStr();
     }
